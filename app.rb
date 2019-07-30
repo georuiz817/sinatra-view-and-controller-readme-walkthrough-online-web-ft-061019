3e44516957aec3e7dd37daece91e7f29fbe5fa1b
George Ruiz <georuiz817@gmail.com>
2019-07-30 21:43:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ class App < Sinatra::Base
   erb :reversed
   end
 
-  get '/friends' do
-    # Write your code here!
 get '/friends' do
   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
  
